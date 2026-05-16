4ef3bebf8bc3cf6d45e3a437cbfe010014bdbd0c
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-05-17 00:37:34 +0530

    feat(studio): implement video upload and studio view skeleton

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,13 +1,43 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { LoaderIcon, PlusIcon } from "lucide-react";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LIMIT } from "@/constants";
+import { useTRPC } from "@/trpc/client";
 
 export const StudioUploadModal = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+  const create = useMutation(
+    trpc.videos.create.mutationOptions({
+      onError: () => {
+        toast.error("Failed to create video");
+      },
+      onSuccess: () => {
+        toast.success("Video created");
+        queryClient.invalidateQueries({
+          queryKey: trpc.studio.getMany.infiniteQueryKey({
+            limit: DEFAULT_LIMIT,
+          }),
+        });
+      },
+    }),
+  );
+
   return (
-    <Button variant="secondary">
-      <PlusIcon />
+    <Button
+      disabled={create.isPending}
+      variant="secondary"
+      onClick={() => create.mutate()}
+    >
+      {create.isPending ? (
+        <LoaderIcon className="animate-spin" />
+      ) : (
+        <PlusIcon />
+      )}
       Create
     </Button>
   );
